f815769489d06601784a33c49dec0abe147c8fa3
Hildah-art <njagahildah@gmail.com>
2025-05-14 10:32:36 +0300

    test the object initialization

--- stray_havens.py
+++ stray_havens.py
@@ -1,11 +1,31 @@
+# Class initialization with default init 
 class Pet:
-    pass
+    def speak(self):
+        return("Pet spoke")
 
+Rasmus = Pet()
+Rasmus.name = "Rasmus"
+print(Rasmus.name)
+print(Rasmus.speak())
+
+# A simple class to represent a dog with proper __init__
 class Dog:
-    pass
+    def __init__(self, name, breed, age):  
+        self.name = name
+        self.breed = breed
+        self.age = age 
+
+    def speak(self):
+        return (f"{self.name} says woof! woof!")
+
+koba = Dog("Koba", "Great Dane", 3)
+koba.age = 4 
+print(koba.speak())
+print(koba.age)
+
 
 class Cat:
     pass
 
-class Chipmunk:
+class Rat:
     pass
