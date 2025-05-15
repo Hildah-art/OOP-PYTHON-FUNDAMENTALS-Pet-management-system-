e03492914a4f4911d55bb892a15bf8a16e9f1d96
Hildah-art <njagahildah@gmail.com>
2025-05-15 10:14:01 +0300

    initialised and added class attributes

---
+++ stray_havens.py
@@ -0,0 +1,33 @@
+# class initialisation with default init 
+class Pet:
+    def speak(self):
+        # print("Sound made !")
+        return("Pet spoke")
+
+Rasmus = Pet()
+Rasmus.name = "Rasmus"
+print(Rasmus.name)
+print(Rasmus.speak())
+ 
+#  A simple class to represent a dog with modified  _init_
+class Dog:
+    species = "Canis_lupaz_familiaris"
+    def _init_(self,name,breed,age="N/A"):
+        self.name = name
+        self.breed = breed
+        self.age = age 
+
+    def speak(self):
+        return (f"{self.name} say woof! woof!")
+        
+koba = Dog("Koba","Great Dane",3)
+amad= Dog ("Amad", "GermanShephard")
+koba.age = 4
+print(koba.speak())
+print(koba.age)
+print(amad.age)
+print(amad.species)==print(koba.species)
+class Cat:
+    pass
+class Rat:
+    pass
